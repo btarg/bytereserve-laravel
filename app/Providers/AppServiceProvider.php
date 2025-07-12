354e4aa369f1e0f5f21878eeb54981a09beca86e
btarg <27726289+btarg@users.noreply.github.com>
2025-07-12 19:30:52 +0100

    error pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
 
         Vite::prefetch(concurrency: 3);
         
+        // Register view namespace hints
+        View::addNamespace('errors', resource_path('views/errors'));
     }
 }
